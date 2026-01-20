4b0e136bfb866260d10164866e7f63925dea83b1
Ahamed-Assal <ahamed.assalk@gmail.com>
2026-01-20 11:36:26 -0500

    Week 02 Lab

--- Week02/lab02.py
+++ Week02/lab02.py
@@ -8,6 +8,7 @@ playerChoice = int(playerChoice)
 
 if playerChoice < 1 or playerChoice > 3:
     print("Error: Choice should be between 1 and 3!")
+    
 else:
     ComputerChoice = random.randint(1,3)
 
@@ -16,11 +17,11 @@ else:
     print(playerChoiceIndex, ComputerChoiceIndex)
 
     # Determine the winner logic using if/elif/else
-    if playerChoice = ComputerChoice:
+    if playerChoice == ComputerChoice:
         print("It's a tie")
-    elif playerChoice = 1 and ComputerChoice = 3:
+    elif playerChoice == 1 and ComputerChoice == 3:
         print("Rock beats scissor - you win!")
-    elif playerChoice = 2 and ComputerChoice = 1:
+    elif playerChoice == 2 and ComputerChoice == 1:
         print("Paper beats Rock - you win!")
     else:
         print("You lose!")
